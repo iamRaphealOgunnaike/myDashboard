8b7d31743ba13b6a6444e8ec392dd61375d1a1e4
ogunraph_lab <scitech4africa@gmail.com>
2023-01-23 13:32:31 -0800

    seond commit

---
+++ client/src/scenes/dashboard/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dashborad = ()=> {
+  return  <div> Dashboard </div>;  
+};
+
+export default Dashborad;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-import express from 'express';
+import express, { application } from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -16,3 +20,22 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+/*ROUTES */
+
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("sales", salesRoutes);
+
+
+/*MANGOOSE SETUP */
+const PORT = process.env.PORT || 5002;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+         app.listen (PORT, ()=> console.log(`Server Port: ${PORT}`));
+}).catch((error)=>console.log(`${error} did not connect`));
